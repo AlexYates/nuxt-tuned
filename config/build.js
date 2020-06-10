bf7c3b20c76b0510759dfbad9ce5b70bc92b66b1
Yates <1916963+AlexYates@users.noreply.github.com>
2020-06-10 02:25:52 +0100

    Chunk split optimisation

--- config/build.js
+++ config/build.js
@@ -2,16 +2,26 @@
 const build = {
   extend(config, { isDev, isClient }) {
     // https://nuxtjs.org/faq/extend-webpack#execute-eslint-on-every-webpack-build-in-dev-environment
-    if (isDev && isClient) {
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.(js|ts|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /(node_modules)/,
-        options: {
-          fix: true
-        }
-      });
+    if (isDev) {
+      if (isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          exclude: /(node_modules)/,
+          loader: 'eslint-loader',
+          options: {
+            fix: true
+          },
+          test: /\.(js|ts|vue)$/
+        })
+      }
+    }
+  },
+  // https://github.com/nuxt/nuxt.js/issues/2201#issuecomment-616691273
+  optimization: {
+    splitChunks: {
+      automaticNameDelimiter: '.',
+      chunks: 'all',
+      maxSize: 51200, // ~< 50kib chunks
     }
   }
 }
