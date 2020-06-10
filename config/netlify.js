a03c161dc7dbec9d87961c1bc8ea1fe04e10b319
Yates <1916963+AlexYates@users.noreply.github.com>
2020-06-11 00:41:16 +0100

    CSP updated, based on https://csp-evaluator.withgoogle.com/

--- config/netlify.js
+++ config/netlify.js
@@ -2,15 +2,55 @@
 const netlify = {
   headers: {
     '/*': [
-      `Content-Security-Policy: default-src 'self' cdn.jsdelivr.net`,
-      `Feature-Policy: accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'self'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none';  picture-in-picture 'none'; speaker 'none'; sync-xhr 'none'; usb 'none'; vr 'none';`,
-      'Referrer-Policy: no-referrer',
-      'X-Content-Type-Options: nosniff',
-      'X-Frame-Options: DENY',
-      'X-XSS-Protection: 1; mode=block',
+      `Content-Security-Policy:
+        default-src 'none';
+        base-uri 'self';
+        connect-src 'self';
+        form-action 'self';
+        img-src 'self';
+        script-src 'unsafe-eval' 'unsafe-inline' 'self' https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/;
+        style-src 'self';
+        require-trusted-types-for 'script';
+      `,
+      `Feature-Policy:
+        accelerometer 'none';
+        ambient-light-sensor 'none';
+        autoplay 'none';
+        camera 'none';
+        encrypted-media 'none';
+        fullscreen 'self';
+        geolocation 'none';
+        gyroscope 'none';
+        magnetometer 'none';
+        microphone 'none';
+        midi 'none';
+        payment 'none';
+        picture-in-picture 'none';
+        speaker 'none';
+        sync-xhr 'none';
+        usb 'none';
+        vr 'none';
+      `,
+      `Referrer-Policy:
+        no-referrer;
+      `,
+      `X-Content-Type-Options:
+        nosniff;
+      `,
+      `X-Frame-Options:
+        DENY;
+      `,
+      `X-XSS-Protection:
+        1;
+        mode=block;
+      `,
     ],
     '/favicon.ico': [
-      'Cache-Control:  public, max-age=31536000, immutable'
+      `Cache-Control:
+        immutable,
+        max-age=31536000,
+        public,
+      `
     ]
   }
 }
