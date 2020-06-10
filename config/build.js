1f5460e1ea14bfc3c4c2e657c20c79c590a3b891
Yates <1916963+AlexYates@users.noreply.github.com>
2020-06-10 20:40:03 +0100

    50kb chunk size

--- config/build.js
+++ config/build.js
@@ -24,8 +24,8 @@ const build = {
     splitChunks: {
       automaticNameDelimiter: '.',
       chunks: 'all',
-      // maxSize: 51200, // ~< 50kib chunks
-      maxSize: 102400, // ~< 100kib chunks
+      maxSize: 51200, // ~< 50kib chunks
+      // maxSize: 102400, // ~< 100kib chunks
     }
   }
 }
