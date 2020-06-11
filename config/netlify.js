e354d9fe26e7abcf7902c09743138e261289dccc
Yates <1916963+AlexYates@users.noreply.github.com>
2020-06-11 11:38:16 +0100

    Simplified header creation. Removed 'require-trusted-type-for' until it leaves experimental.

--- config/netlify.js
+++ config/netlify.js
@@ -1,56 +1,51 @@
+/**
+ * @name headerCreate
+ * @param {string} name
+ * @param {Array<string>} rules
+ * @returns {string}
+ */
+const headerCreate = (name, rules) => `${name}: ${rules.join('; ')}`
+
 // https://github.com/aceforth/nuxt-netlify#headers
 const netlify = {
   headers: {
     '/*': [
-      `Content-Security-Policy:
-        default-src 'none';
-        base-uri 'self';
-        connect-src 'self';
-        form-action 'self';
-        img-src 'self';
-        script-src 'unsafe-eval' 'unsafe-inline' 'self' https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/;
-        style-src 'self';
-        require-trusted-types-for 'script';
-      `,
-      `Feature-Policy:
-        accelerometer 'none';
-        ambient-light-sensor 'none';
-        autoplay 'none';
-        camera 'none';
-        encrypted-media 'none';
-        fullscreen 'self';
-        geolocation 'none';
-        gyroscope 'none';
-        magnetometer 'none';
-        microphone 'none';
-        midi 'none';
-        payment 'none';
-        picture-in-picture 'none';
-        speaker 'none';
-        sync-xhr 'none';
-        usb 'none';
-        vr 'none';
-      `,
-      `Referrer-Policy:
-        no-referrer;
-      `,
-      `X-Content-Type-Options:
-        nosniff;
-      `,
-      `X-Frame-Options:
-        DENY;
-      `,
-      `X-XSS-Protection:
-        1;
-        mode=block;
-      `,
+      headerCreate('Content-Security-Policy', [
+        `default-src 'none'`,
+        `base-uri 'self'`,
+        `connect-src 'self'`,
+        `form-action 'self'`,
+        `img-src 'self'`,
+        `script-src 'unsafe-eval' 'unsafe-inline' 'self' https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/`,
+        `style-src 'self'`,
+        // `require-trusted-types-for 'script'`, // Experimental API
+      ]),
+      headerCreate('Feature-Policy', [
+        `accelerometer 'none'`,
+        `ambient-light-sensor 'none'`,
+        `autoplay 'none'`,
+        `camera 'none'`,
+        `encrypted-media 'none'`,
+        `fullscreen 'self'`,
+        `geolocation 'none'`,
+        `gyroscope 'none'`,
+        `magnetometer 'none'`,
+        `microphone 'none'`,
+        `midi 'none'`,
+        `payment 'none'`,
+        `picture-in-picture 'none'`,
+        `speaker 'none'`,
+        `sync-xhr 'none'`,
+        `usb 'none'`,
+        `vr 'none'`,
+      ]),
+      headerCreate('Referrer-Policy', ['no-referrer']),
+      headerCreate('X-Content-Type-Options', ['nosniff']),
+      headerCreate('X-Frame-Options', ['DENY']),
+      headerCreate('X-XSS-Protection', ['1', 'mode=block']),
     ],
     '/favicon.ico': [
-      `Cache-Control:
-        immutable,
-        max-age=31536000,
-        public,
-      `
+      headerCreate('Cache-Control', ['immutable', 'max-age=31536000', 'public']),
     ]
   }
 }
