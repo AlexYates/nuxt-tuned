d7a80e889e5c0abb08d7eddf90fd5308558892d1
Yates <1916963+AlexYates@users.noreply.github.com>
2020-07-04 16:13:40 +0100

    [chore]

--- config/netlify.js
+++ config/netlify.js
@@ -19,11 +19,11 @@ const netlify = {
         `form-action 'self'`,
         `img-src 'self'`,
         `manifest-src 'self'`,
+        // https://web.dev/trusted-types/
+        // `require-trusted-types-for 'script'`,
         `script-src 'self' 'sha256-sH4yPnnlZ9QdUT1I/FPmXXy51PSFhE35uqKJFXMik/4=' 'strict-dynamic' 'unsafe-eval' 'unsafe-inline'  https:`,
         `style-src 'self' 'unsafe-inline'`,
         `script-src-elem 'self' https:`,
-        // https://web.dev/trusted-types/
-        // `require-trusted-types-for 'script'`,
       ]),
       headerCreate('Content-Security-Policy-Report-Only', [
         // https://web.dev/trusted-types/
